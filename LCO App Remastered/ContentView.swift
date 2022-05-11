af4c1f574d9a2bb6243ed37edc956f3b835a079c
ankanbhattacharya <ankanbhattachatya89@gmail.com>
2022-05-11 17:05:39 +0530

    Initial Commit

---
+++ LCO App Remastered/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LCO App Remastered
+//
+//  Created by Ankan Bhattacharya on 11/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LCO App Remastered/LCO_App_RemasteredApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCO_App_RemasteredApp.swift
+//  LCO App Remastered
+//
+//  Created by Ankan Bhattacharya on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct LCO_App_RemasteredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
